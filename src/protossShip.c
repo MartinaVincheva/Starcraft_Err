3c6e7aed8a7f7b930c6cedb8c1cb7b3e2aa31bc0
Martina Vincheva <tufituri@gmail.com>
2021-05-13 11:07:43 +0300

    Err_task

--- src/protossShip.c
+++ src/protossShip.c
@@ -7,15 +7,37 @@ MAX_FLEET_SIZE = 100;
 
 static void printProtossShip(ship *myShip)
 {
+    if (NULL == myShip)
+    {
+        perror("Function printProtossShip pointer is null\n");
+        return NULL;
+    }
     protossShip *pShip = (protossShip *)myShip;
+
+    if (!pShip)
+    {
+        perror("Failed to cast !\n");
+        return NULL;
+    }
 
     printf("Last Protoss AirShip with ID: %d has %d health and %d shield left\n", pShip->item.index, pShip->item.health, pShip->shield);
 }
 
 static bool protossUnderAttack(ship *attacker, ship *attacked)
 {
+    if (NULL == attacker || NULL == attacked)
+    {
+        perror("Function protossUnderAttack pointer is null\n");
+        return NULL;
+    }
     int attack_strenght = attacker->attackStrength(attacker, attacked);
     protossShip *myShip = (protossShip *)attacked;
+
+    if (!myShip)
+    {
+        perror("Failed to cast !\n");
+        return NULL;
+    }
 
     if (attack_strenght <= myShip->shield)
     {
@@ -32,6 +54,12 @@ static bool protossUnderAttack(ship *attacker, ship *attacked)
 
 static void recoverPhoenixShield(protossShip *myShip)
 {
+    if (NULL == myShip)
+    {
+        perror("Function printProtossShip pointer is null\n");
+        return NULL;
+    }
+
     myShip->shield += PHOENIX_SHIELD_REGENERATE_RATE;
     if (myShip->shield > PHOENIX_SHIELD)
     {
@@ -41,12 +69,23 @@ static void recoverPhoenixShield(protossShip *myShip)
 
 static int phoenixAttacksCount(protossShip *myShip)
 {
+    if (NULL == myShip)
+    {
+        perror("Function printProtossShip pointer is null\n");
+        return NULL;
+    }
     (void)myShip;
     return (1);
 }
 
 static int phoenixAttackStrength(ship *attacker, ship *attacked)
 {
+    if (NULL == attacker || NULL == attacked)
+    {
+        perror("Function protossUnderAttack pointer is null\n");
+        return NULL;
+    }
+
     (void)attacker;
     (void)attacked;
     return (PHOENIX_DAMAGE);
@@ -57,19 +96,17 @@ static char *phoenixTypeToString(void)
     return ("Phoenix");
 }
 
-
 ship *createPhoenixShip(int index)
 {
-    if !((index >= 0) && (index < MAX_FLEET_SIZE))
+    if (!((index >= 0) && (index < MAX_FLEET_SIZE)))
     {
-        errno = INDEX_OUT_OF_BOUNDS;
+        perror("Index is out of bounds!\n");
         return NULL;
     }
     protossShip *s = (protossShip *)malloc(sizeof(protossShip));
 
     if (!s)
     {
-        errno = MEMOMRY_ALLOCATION_FAIL;
         perror("Unsuccessful memory allocation for protossShip\n");
         return NULL;
     }
@@ -91,8 +128,8 @@ static void recoverCarrierShield(protossShip *myShip)
 {
     if (NULL == myShip)
     {
-        perror("Can't recover CarrierShield!\n");
-        exit(1);
+        perror("Function recoverCarrierShield pointer is null\n");
+        return NULL;
     }
     myShip->shield += CARRIER_SHIELD_REGENERATE_RATE;
     if (myShip->shield > CARRIER_SHIELD)
@@ -103,6 +140,11 @@ static void recoverCarrierShield(protossShip *myShip)
 
 static int carrierAttacksCount(protossShip *myShip)
 {
+    if (NULL == myShip)
+    {
+        perror("Function carrierAttacksCount pointer is null\n");
+        return NULL;
+    }
     if (myShip->item.health < CARRIER_HEALTH)
     {
         return (DAMAGED_STATUS_INTERCEPTORS);
@@ -114,7 +156,7 @@ static int carrierAttackStrength(ship *attacker, ship *attacked)
 {
     if (NULL == attacker || NULL == attacked)
     {
-        perror("Function carrierAttackStrength pointer is null");
+        perror("Function carrierAttackStrength pointer is null\n");
         return NULL;
     }
     (void)attacker;
@@ -129,7 +171,19 @@ static char *carrierTypeToString(void)
 
 ship *createCarrierShip(int index)
 {
+    if (!((index >= 0) && (index < MAX_FLEET_SIZE)))
+    {
+        perror("Index is out of bounds!");
+        return NULL;
+    }
+
     protossShip *s = (protossShip *)malloc(sizeof(protossShip));
+
+    if (!s)
+    {
+        perror("Unsuccessful memory allocation for protossShip\n");
+        return NULL;
+    }
 
     s->item.type = CARRIER;
     s->item.index = index;
